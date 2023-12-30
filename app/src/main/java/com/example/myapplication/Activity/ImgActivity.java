930c9d76f7dd6feb4cafebac5b5d796367a7d7b0
SHR <shr9204@kaist.ac.kr>
2023-12-30 10:33:38 +0900

    2023-12-30

--- app/src/main/java/com/example/myapplication/Activity/ImgActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/ImgActivity.java
@@ -1,120 +1,96 @@
 package com.example.myapplication.Activity;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import com.example.myapplication.R;
 
 public class ImgActivity extends AppCompatActivity {
+    ImageView imageView;
+    String imgName = "osz.png";    // 이미지 이름
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_img);
-        setTitle("list view exam");
 
-        final GridView gv = findViewById(R.id.gridView);
-        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        imageView = findViewById(R.id.imageView);
 
-        gv.setAdapter(gAdapter);
-
-        Button mainButton=findViewById(R.id.mainbut);
-        mainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        try {
+            String imgpath = getCacheDir() + "/" + imgName;   // 내부 저장소에 저장되어 있는 이미지 경로
+            Bitmap bm = BitmapFactory.decodeFile(imgpath);
+            imageView.setImageBitmap(bm);   // 내부 저장소에 저장된 이미지를 이미지뷰에 셋
+            Toast.makeText(getApplicationContext(), "파일 로드 성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 로드 실패", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public class MyGridAdapter extends BaseAdapter{
-        Context context;
-        public MyGridAdapter(Context c){
-            context = c;
-        }
+    public void bt1(View view) {    // 이미지 선택 누르면 실행됨 이미지 고를 갤러리 오픈
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 101);
+    }
 
-        // 실제 이미지는 10개이지만, 갤러리에 풍성하게 보이기 위해
-        // 세 번 반복해서 붙여넣었습니다.
-        Integer[] picID = {
-                R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
-                R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10,
-                R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
-                R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10,
-                R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
-                R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10
-        };
-
-        // BaseAdapter를 상속받은 클래스가 구현해야 할 함수들은
-        // { getCount(), getItem(), getItemId(), getView() }
-        // Ctrl + i 를 눌러 한꺼번에 구현할 수 있습니다.
-        @Override
-        public int getCount() {
-            return picID.length;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            ImageView imageView = new ImageView(context);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(200,300));
-            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imageView.setPadding(5,5,5,5);
-
-            imageView.setImageResource(picID[i]);
-
-
-
-            // 갤러리의 이미지뷰를 눌렀을 때
-            // 다이얼로그뷰를 팝업하여 큰 이미지를 출력합니다.
-            final int pos = i;
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    View dialogView = View.inflate(ImgActivity.this, R.layout.dialog, null);
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(ImgActivity.this);
-                    ImageView ivPic = dialogView.findViewById(R.id.ivPic);
-                    ivPic.setImageResource(picID[pos]);
-                    dlg.setTitle("큰 이미지");
-                    dlg.setIcon(R.drawable.ic_launcher_background);
-                    dlg.setView(dialogView);
-                    dlg.setNegativeButton("닫기", null);
-                    dlg.show();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // 갤러리
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    imageView.setImageBitmap(imgBitmap);    // 선택한 이미지 이미지뷰에 셋
+                    instream.close();   // 스트림 닫아주기
+                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 성공", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 실패", Toast.LENGTH_SHORT).show();
                 }
-            });
+            }
+        }
+    }
 
-            return imageView;
+    public void saveBitmapToJpeg(Bitmap bitmap) {   // 선택한 이미지 내부 저장소에 저장
+        File tempFile = new File(getCacheDir(), imgName);    // 파일 경로와 이름 넣기
+        try {
+            tempFile.createNewFile();   // 자동으로 빈 파일을 생성하기
+            FileOutputStream out = new FileOutputStream(tempFile);  // 파일을 쓸 수 있는 스트림을 준비하기
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);   // compress 함수를 사용해 스트림에 비트맵을 저장하기
+            out.close();    // 스트림 닫아주기
+            Toast.makeText(getApplicationContext(), "파일 저장 성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 저장 실패", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void bt2(View view) {    // 이미지 삭제
+        try {
+            File file = getCacheDir();  // 내부저장소 캐시 경로를 받아오기
+            File[] flist = file.listFiles();
+            for (int i = 0; i < flist.length; i++) {    // 배열의 크기만큼 반복
+                if (flist[i].getName().equals(imgName)) {   // 삭제하고자 하는 이름과 같은 파일명이 있으면 실행
+                    flist[i].delete();  // 파일 삭제
+                    Toast.makeText(getApplicationContext(), "파일 삭제 성공", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 삭제 실패", Toast.LENGTH_SHORT).show();
         }
     }
 }
-
